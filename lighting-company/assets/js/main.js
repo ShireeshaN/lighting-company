eeba23544dfc22319e05b2e7686495427573bfd2
shireeshaN <shireeshanallamasa@gmail.com>
2025-05-12 11:55:08 +0530

    first commit

---
+++ lighting-company/assets/js/main.js
@@ -0,0 +1,124 @@
+$(function() {
+    // 图片lazy加载
+    $(".lazy").lazyload();
+    window.addEventListener('scroll', function(event) {
+        var depth, layer, layers, movement, topDistance, translate3d, _i, _len;
+        topDistance = this.pageYOffset;
+        // console.log(topDistance);
+        layers = document.querySelectorAll("[data-type='parallax']");
+        // console.log(layers);
+        for (_i = 0, _len = layers.length; _i < _len; _i++) {
+            layer = layers[_i];
+            depth = layer.getAttribute('data-depth');
+            movement = -(topDistance * depth);
+            translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+            layer.style['-webkit-transform'] = translate3d;
+            layer.style['-moz-transform'] = translate3d;
+            layer.style['-ms-transform'] = translate3d;
+            layer.style['-o-transform'] = translate3d;
+            layer.style.transform = translate3d;
+        }
+    });
+})
+
+$(function() {
+    scrollAni();
+    $(".m-bocweb-header3 .header2 .hamburger").on("click", function() {
+        if ($(this).hasClass("is-active")) {
+            $(this).removeClass("is-active");
+            $(".m-bocweb-header3 .warp-nav").stop(true, false).hide(0);
+            $("body,html").removeClass("ovh");
+            $(".m-bocweb-header3 .header2 .nav .li").removeClass("animate");
+            if (!$(".m-bocweb-header3").hasClass("on")) {
+                $(".m-bocweb-header3").removeClass("active");
+            }
+            $(".header2 .h-right span").html("MENU");
+        } else {
+            $(this).addClass("is-active");
+            $(".m-bocweb-header3 .warp-nav").stop(true, false).show(0);
+            $("body,html").addClass("ovh");
+            $(".m-bocweb-header3 .header2 .nav .li").addClass("animate");
+            if (!$(".m-bocweb-header3").hasClass("on")) {
+                $(".m-bocweb-header3").addClass("active");
+            }
+            $(".header2 .h-right span").html("COLSE");
+        }
+    })
+    var _colSpan = $('.m-bocweb-header3 .header2 .nav .li');
+    var _len = _colSpan.length;
+    var i = 0;
+    for (i; i < _len; i++) {
+        _colSpan.eq(i).css({
+            'transition-delay': 0.1 + 0.04 * (i + 1) + 's'
+        });
+    }
+
+
+    $(".m-bocweb-header3 .header2 .sub-tit").on('click', function() {
+        if ($(this).siblings('.sec-list').is(':hidden')) {
+            $(this).addClass('on');
+            $(this).siblings('.sec-list').stop().slideDown();
+            $(this).parent().siblings('li').children('.sec-list').stop().slideUp().siblings('.tit').removeClass('on');
+        } else {
+            $(this).removeClass('on');
+            $(this).siblings('.sec-list').stop().slideUp();
+        }
+    });
+
+    $(".searchbtn").on('click', function() {
+        $(".search-alert2").stop().fadeIn(300);
+        $("body,html").addClass('ovh');
+    });
+    $(".search-alert2 .close").on('click', function() {
+        $(".search-alert2").stop().hide();
+        $("body,html").removeClass('ovh');
+    });
+
+    var target_top = $(window).scrollTop();
+    if (target_top > 100) {
+        $(".warp-header4").addClass("active");
+    } else {
+        $(".warp-header4").removeClass("active");
+    }
+    $(window).scroll(function() {
+        var target_top = $(window).scrollTop();
+
+        if (target_top > 100) {
+            $(".warp-header4").addClass("active");
+        } else {
+            $(".warp-header4").removeClass("active");
+        }
+    });
+
+    backTop($(".backbtn"));
+
+
+    $(".header4 .leftbox .li").hover(function() {
+        $(this).find(".sub-list").stop().slideDown();
+    }, function() {
+        $(this).find(".sub-list").stop().slideUp();
+    })
+})
+//页面加载
+// $('#loading').imagesLoaded({ background: true })
+// .progress( function( instance, image ) {    //progress事件，在每一张图片加载完成时都触发一次
+//     var box=$('#num');
+//     imgNum = (imgNum+1)>=100?100:(imgNum+1);
+//     box.text(imgNum);
+// })
+// .always( function( instance ) {
+//     var box=$('#num');
+//     var timer = setInterval(function () {
+//         if(imgNum<100){
+//             imgNum += Math.round(Math.random());
+//             box.text(imgNum);
+//         }else{
+//             setTimeout(function () {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+//                 $('#loading').fadeOut(1500,function () {
+
+//                 });
+//             },1000);
+//             clearInterval(timer);
+//         }
+//     },10);
+// });
